7549072a2ae956826154886f6223702b3948c8eb
Clara Lecroisey <claralecroisey@MacBook-Pro-de-Clara.local>
2023-08-22 12:07:47 +0200

    feat: add levels

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,40 @@ import { useState } from 'react';
 import './App.css';
 import { Grid } from './Grid';
 
+enum Level {
+  Beginner = 'Beginner',
+  Intermediate = 'Intermediate',
+  Expert = 'Expert'
+}
+
+const LEVELS = {
+  [Level.Beginner]: { N: 9, M: 9, minesCount: 10 },
+  [Level.Intermediate]: { N: 16, M: 16, minesCount: 40 },
+  [Level.Expert]: { N: 30, M: 30, minesCount: 99 }
+};
+
 function App() {
-  const [n, setN] = useState(3);
-  const [m, setM] = useState(3);
+  const [level, setLevel] = useState<Level>(Level.Beginner);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Minesweeper</h1>
       </header>
-      <div>
-        <input
-          type="number"
-          value={n}
-          onChange={(e) => {
-            setN(+e.target.value);
-          }}
-        />
-        <input
-          type="number"
-          value={m}
-          onChange={(e) => {
-            setM(+e.target.value);
-          }}
-        />
+      <div className="LevelButtonsContainer">
+        {Object.keys(LEVELS).map((l) => (
+          <button
+            key={l}
+            onClick={() => {
+              setLevel(l as Level);
+            }}>
+            {l}
+          </button>
+        ))}
       </div>
       <br />
       <div className="Grid">
-        <Grid N={n} M={m} NB_BOMBS={n - 1} />
+        <Grid N={LEVELS[level].N} M={LEVELS[level].N} NB_BOMBS={LEVELS[level].minesCount} />
       </div>
     </div>
   );
